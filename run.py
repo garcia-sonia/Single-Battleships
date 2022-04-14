bad78615169102054a32569fdf2987bfba181e3d
Sonia Garcia <soniagarciapascual@gmail.com>
2022-04-14 13:20:34 +0000

    Add conversion letters to numbers for 8 rows from A to H

--- run.py
+++ run.py
@@ -5,3 +5,4 @@ MY_BOARD = [[' '] * 8 for x in range(8)]
 # Board for holding our guesses including hits and misses
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
+letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
