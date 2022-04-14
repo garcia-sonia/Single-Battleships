7c2e2b11b4acca8cb6b4ced81d9f41ba921f56ca
Sonia Garcia <soniagarciapascual@gmail.com>
2022-04-14 13:25:44 +0000

    Create function to ask user where they want to place ships

--- run.py
+++ run.py
@@ -7,4 +7,11 @@ GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
 letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
-def board()
+def board():
+    pass
+
+''' 
+Function to ask user what column and row they want to place the ship on
+'''
+def get_ship_location():
+    pass
