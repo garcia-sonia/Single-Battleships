35f8157b0f685fb3ad305780b2565d210dd34bce
Sonia Garcia <soniagarciapascual@gmail.com>
2022-04-14 13:42:50 +0000

    Define print_board function and format board

--- run.py
+++ run.py
@@ -7,8 +7,13 @@ GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
 letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
-def board():
-    pass
+def print_board(board):
+    print('     A B C D E F G H')
+    print('     ---------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s" % (row_number, "|".join(row)))
+        row_number +=1
 
 ''' 
 Function to create the ships
@@ -31,3 +36,7 @@ Function to count how many times the ship is hit and game over when 5 hits
 
 def count_hit_ships():
     pass
+
+create_ships()
+turns=10
+# while turns > 0:
