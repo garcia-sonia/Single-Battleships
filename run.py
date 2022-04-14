57be5eefac785aa16b095769eca10418b889a7dd
Sonia Garcia <soniagarciapascual@gmail.com>
2022-04-14 14:35:03 +0000

    Removing spaces from in front of letters for columns to better center letters with columns when boards are printed

--- run.py
+++ run.py
@@ -17,8 +17,8 @@ GUESS_BOARD = [[' '] * 8 for i in range(8)]
 letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
 def print_board(board):
-    print('     A B C D E F G H')
-    print('     ---------------')
+    print('  A B C D E F G H')
+    print('  ---------------')
     row_number = 1
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
